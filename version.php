bedf3f052502e5ef168d35681cda972cd5bd6dee
Henix <hen.aton@gmail.com>
2022-06-18 11:18:59 +0300

    Update version.php

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2022061800;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release  = '3.0.5';
+$plugin->release  = '3.0.6';
 $plugin->requires  = 2016052300;        // Requires this Moodle version 3.1.
 $plugin->component = 'local_annoto';    // Full name of the plugin (used for diagnostics).
