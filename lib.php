f08a01f6a0182191c927c83cf597f4425e398d64
Ilya Dymshits <ilya@devlion.co>
2023-05-02 12:09:09 +0300

    (update) widget position for moodle4

--- lib.php
+++ lib.php
@@ -228,6 +228,7 @@ function local_annoto_get_jsparam($courseid, $modid) {
         'mediaGroupTitle' => $course->fullname,
         'mediaGroupDescription' => $course->summary,
         'locale' => $settings->locale ? local_annoto_get_lang($course) : false,
+        'moodleversion' => $CFG->version,
     );
 
     return $jsparams;
