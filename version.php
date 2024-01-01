62b6992f58cfd28cde734d15e5c15079336a88c3
genadis <genadis.pub@gmail.com>
2024-01-01 02:08:31 +0200

    chore: release 5.0.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2023122700;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release  = '4.0.10';
+$plugin->version   = 2024010100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release  = '5.0.0';
 $plugin->requires  = 2016052300;        // Requires this Moodle version 3.1.
 $plugin->component = 'local_annoto';    // Full name of the plugin (used for diagnostics).
