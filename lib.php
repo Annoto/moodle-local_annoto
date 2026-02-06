0f3a725ee0ef6af8c18b26fa23d84d1fd4493f55
Stephan Robotta <stephan.robotta@bfh.ch>
2026-02-06 13:22:44 +0100

    Fix use of undefined settings property.

--- lib.php
+++ lib.php
@@ -141,11 +141,11 @@ function local_annoto_get_user_token($settings, $courseid) {
         "name" => fullname($USER), // User's fullname in Moodle.
         "email" => $USER->email, // User's email.
         "photoUrl" => is_object($userpictureurl) ? $userpictureurl->out() : '', // User's avatar in Moodle.
-        "iss" => $settings->clientid, // ClientID from global settings.
+        "iss" => $settings->clientid ?? '', // ClientID from global settings.
         "exp" => $expire, // JWT token expiration time.
         "scope" => local_annoto_get_user_scope($settings, $courseid),
     ];
-    $enctoken = \Firebase\JWT\JWT::encode($payload, $settings->ssosecret, 'HS256');
+    $enctoken = \Firebase\JWT\JWT::encode($payload, $settings->ssosecret ?? '', 'HS256');
 
     return $enctoken;
 }
@@ -263,8 +263,8 @@ function local_annoto_get_jsparam($courseid, $modid) {
 
     $jsparams = [
         'deploymentDomain' => local_annoto_get_deployment_domain(),
-        'bootstrapUrl' => $settings->scripturl,
-        'clientId' => $settings->clientid,
+        'bootstrapUrl' => $settings->scripturl ?? '',
+        'clientId' => $settings->clientid ?? '',
         'userToken' => local_annoto_get_user_token($settings, $courseid),
         'loginUrl' => $loginurl,
         'logoutUrl' => $logouturl,
